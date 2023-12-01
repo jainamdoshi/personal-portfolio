f445337b470c81c49e5d31dfd247dbbffd6d0a1a
jainamdoshi <jainam.doshi2001@gmail.com>
2023-12-02 01:03:25 +1100

    Add text shadow

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -20,7 +20,7 @@ export default function Projects(props: { projects: ProjectProps[] }) {
                         <div className="w-[25rem] h-[20rem] my-3">
                             <div key={project.title} className="group bg-pink-red rounded-2xl overflow-hidden h-[15rem] relative">
                                 <img src={project.coverImage} alt={project.title} className="h-full w-full" />
-                                <h3 className="absolute bottom-0 text-white font-extrabold text-lg p-3 w-full text-center">{project.title}</h3>
+                                <h3 className="absolute bottom-0 text-white font-extrabold text-lg p-3 w-full text-center text-shadow-black">{project.title}</h3>
                                 <div className="absolute top-0 left-0 w-full h-full bg-pink-red flex flex-col items-center scale-0 group-hover:scale-100 justify-between py-5 px-3">
                                     <h4 className="text-white font-extrabold text-3xl text-center">{project.subTitle}</h4>
                                     <p className="text-white font-normal text-md text-center">{project.description}</p>
@@ -28,7 +28,7 @@ export default function Projects(props: { projects: ProjectProps[] }) {
                                         href={`${project.url}`}
                                         rel="noopener noreferrer"
                                         target="_blank"
-                                        className="hover:bg-white hover:text-pink-red font-semibold px-5 py-2 rounded-lg text-white border-2"
+                                        className="hover:bg-white hover:text-pink-red font-semibold px-5 py-2 rounded-lg text-white border-2 border-white duration-200"
                                     >
                                         View Project
                                     </Link>
