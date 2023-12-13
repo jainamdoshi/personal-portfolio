84e4f0909371937ab79ef4f74537a75a5b6cac5f
jainamdoshi <jainam.doshi2001@gmail.com>
2023-12-14 10:48:37 +1100

    Fix formatting in Projects component

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -8,7 +8,7 @@ export type ProjectProps = {
     description: string;
     coverImage: string;
     url: string;
-    tags?: string[]
+    tags?: string[];
 };
 
 export default function Projects(props: { projects: ProjectProps[] }) {
@@ -18,9 +18,9 @@ export default function Projects(props: { projects: ProjectProps[] }) {
             <div className="flex flow-row justify-around my-10 px-16 flex-wrap">
                 {props.projects.map((project) => {
                     return (
-                        <div key={project.title} className="w-[20rem] my-3">
+                        <div key={project.title} className="w-[20rem] my-3 mx-2">
                             <div key={project.title} className="group bg-pink-red rounded-2xl overflow-hidden h-[15rem] relative">
-                                <Image src={project.coverImage} alt={project.title} className="h-full w-full" width={0} height={0}/>
+                                <Image src={project.coverImage} alt={project.title} className="h-full w-full" width={0} height={0} />
                                 <h3 className="absolute bottom-0 text-white font-extrabold text-lg p-3 w-full text-center text-shadow-black">{project.title}</h3>
                                 <div className="absolute top-0 left-0 w-full h-full bg-pink-red flex flex-col items-center scale-0 group-hover:scale-100 justify-between py-5 px-3">
                                     <h4 className="text-white font-extrabold text-3xl text-center">{project.subTitle}</h4>
@@ -38,7 +38,12 @@ export default function Projects(props: { projects: ProjectProps[] }) {
                             <div className="flex flex-row flex-wrap items-center px-2 mt-2 w-full">
                                 {project.tags?.map((tag) => {
                                     return (
-                                        <span key={tag} className="bg-transparent text-pink-red-light text-sm font-light px-2 py-1 rounded-lg mr-2 border border-pink-red-light my-1">{tag}</span>
+                                        <span
+                                            key={tag}
+                                            className="bg-transparent text-pink-red-light text-sm font-light px-2 py-1 rounded-lg mr-2 border border-pink-red-light my-1"
+                                        >
+                                            {tag}
+                                        </span>
                                     );
                                 })}
                             </div>
