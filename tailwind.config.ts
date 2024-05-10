654dc9b1cf5de91b625451bb9c4209cdc1b45f4f
jainamdoshi <jainam.doshi2001@gmail.com>
2024-05-10 08:34:49 +0800

    Add shadcn ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,52 +1,111 @@
 import type { Config } from 'tailwindcss';
 import plugin from 'tailwindcss/plugin';
 
-const config: Config = {
-    content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
-    theme: {
-        extend: {
-            backgroundImage: {
-                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-                'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-                code: "linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.90), rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url('https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80')",
-            },
-            backgroundSize: {
-                code: '84% 100%',
-            },
-            colors: {
-                'pink-red': '#f5004e',
-                'pink-red-light': '#f61a60',
-                grey: 'rgba(198,201,216, 0.75)',
-            },
-            textShadow: {
-                'pink-red': '-0.15rem 0.15rem 0.15rem #f5004e',
-                black: '0.1rem 0.1rem 0.65rem #000',
-            },
-            keyframes: {
-                scale: {
-                    '0%': { transform: 'scale(1)' },
-                    '100%': { transform: 'scale(1.1)' },
-                },
-            },
-            animation: {
-                scale: 'scale 1.5s ease-in-out infinite alternate',
-            },
-            boxShadow: {
-                'pink-red': '0.35rem 0.15rem 1.25rem #f5004e',
-            },
-        },
-    },
-    plugins: [
-        plugin(function ({ matchUtilities, theme }) {
-            matchUtilities(
-                {
-                    'text-shadow': (value) => ({
-                        textShadow: value,
-                    }),
-                },
-                { values: theme('textShadow') }
-            );
-        }),
-    ],
-};
+const config = {
+	darkMode: ['class'],
+	content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
+	prefix: '',
+	theme: {
+		container: {
+			center: true,
+			padding: '2rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		extend: {
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				},
+				'pink-red': '#f5004e',
+				'pink-red-light': '#f61a60',
+				grey: 'rgba(198,201,216, 0.75)'
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+				code: "linear-gradient(to right, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0.90), rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.75), rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0)), url('https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80')"
+			},
+			backgroundSize: {
+				code: '84% 100%'
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			},
+			textShadow: {
+				'pink-red': '-0.15rem 0.15rem 0.15rem #f5004e',
+				black: '0.1rem 0.1rem 0.65rem #000'
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' }
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' }
+				},
+				scale: {
+					'0%': { transform: 'scale(1)' },
+					'100%': { transform: 'scale(1.1)' }
+				}
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
+				scale: 'scale 1.5s ease-in-out infinite alternate'
+			},
+			boxShadow: {
+				'pink-red': '0.35rem 0.15rem 1.25rem #f5004e'
+			}
+		}
+	},
+	plugins: [
+		require('tailwindcss-animate'),
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					'text-shadow': (value) => ({
+						textShadow: value
+					})
+				},
+				{ values: theme('textShadow') }
+			);
+		})
+	]
+} satisfies Config;
+
 export default config;
